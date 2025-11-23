600ce80a2126d67ac299f931a88a612aa6d2fc64
34rin <34tyunn@gmail.com>
2025-11-23 21:15:10 +0900

    ヘッダーパーツ化・ハンバーガーメニュー／プルダウンメニューCSSだけで実装

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,6 @@
 /* テスト */
 // console.log('JSファイル読み込まれました');
 
-/* ハンバーガーメニュー */
-const hamburgermenu = document.getElementById("hamburgermenu");
-const hamburgermenunav = document.getElementById("hamburgermenunav");
-
-hamburgermenu.addEventListener("click", () => {
-  hamburgermenu.classList.toggle("active");
-  hamburgermenunav.classList.toggle("active");
-});
-
-/* プルダウンメニュー */
-document.querySelectorAll(".pull-down-menu").forEach((menu) => {
-  menu.addEventListener("click", (e) => {
-    e.stopPropagation();
-
-    const nextLi = menu.nextElementSibling; // 次の<li>を取得
-    const submenu = nextLi?.querySelector(".sub-menu");
-
-    if (!submenu) return; // sub-menuが存在しなければ何もしない
-
-    const isOpen = menu.classList.toggle("open");
-    submenu.style.display = isOpen ? "inline-block" : "none";
-  });
-});
-
-document.querySelectorAll(".afi-nav-pdm > span").forEach((menu) => {
-  menu.addEventListener("click", (e) => {
-    e.stopPropagation();
-
-    const submenu = menu.parentElement.querySelector(".afi-nav-sm"); // 同じ<li>内の<ul>を取得
-    if (!submenu) return;
-
-    const isOpen = menu.classList.toggle("open"); //spanにopen付ける
-    submenu.style.display = isOpen ? "inline-block" : "none";
-  });
-});
-
 /* ===== トップページだけ実行 ===== */
 if (location.pathname === "/" || location.pathname.endsWith("index.html")) {
   /* スライドショー */
